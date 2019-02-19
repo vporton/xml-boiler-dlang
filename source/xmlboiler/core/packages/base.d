caac8e6e9d8da6a14ddc3f6c0b1448ccd5b6ecd2
Victor Porton <porton@narod.ru>
2019-02-20 01:01:48 +0200

    implemented struct InstalledSoftwareOptions

--- source/xmlboiler/core/packages/base.d
+++ source/xmlboiler/core/packages/base.d
@@ -20,3 +20,18 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 module xmlboiler.core.packages.base;
 
+import std.typecons;
+
+interface Version {
+    static Version create(string v);
+    // opCmp() from Object
+}
+
+interface BasePackageManaging {
+    /**
+    Return `isNull` if not installed.
+    The version is stripped any distro-specifics. So it is "1.1.29" not "1.1.29-5".
+    */
+    Nullable!string determine_package_version(string packageName);
+    Version createVersion(string v);
+}
