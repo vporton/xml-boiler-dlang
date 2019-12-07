bbb091646858c4b81bcc75475d70a3328a5f6ba8
Victor Porton <porton@narod.ru>
2019-12-07 09:46:35 +0200

    VersionValue class

--- source/xmlboiler/core/packages/base.d
+++ source/xmlboiler/core/packages/base.d
@@ -21,6 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 module xmlboiler.core.packages.base;
 
 import std.typecons;
+import std.conv;
 
 interface Version {
     static Version create(string v);
@@ -34,4 +35,41 @@ interface BasePackageManaging {
     */
     Nullable!string determine_package_version(string packageName);
     Version createVersion(string v);
+}
+
+private int compareVersionNumbers(string a, string b) {
+    import std.ascii, std.algorithm.searching, std.algorithm.comparison;
+
+    // If not numeric, compare lexigraphically:
+    if (!all!isDigit(a) || !all!isDigit(b))
+        return cmp(a, b);
+    else {
+        immutable an = to!long(a);
+        immutable bn = to!long(b);
+        if (an == bn) return 0;
+        return an < bn ? -1 : 1;
+    }
+}
+
+int compareVersions(string a, string b) {
+    import std.array;
+    auto aSplit = a.split('.');
+    auto bSplit = b.split('.');
+    for (auto i = aSplit.front, j = bSplit.front; !aSplit.empty && !bSplit.empty; aSplit.popFront, bSplit.popFront) {
+        immutable comp = compareVersionNumbers(i, j);
+        if (comp != 0) return comp;
+    }
+    if (aSplit.empty && bSplit.empty) return 0;
+    return aSplit.empty ? -1 : 1;
+}
+
+struct VersionValue {
+    this (string _value) {
+        value = _value;
+    }
+    alias value this;
+    int opCmp(VersionValue other) {
+        return compareVersions(this, other);
+    }
+    private string value;
 }
