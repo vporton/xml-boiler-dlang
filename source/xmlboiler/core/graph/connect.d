e1be8b9585129170241301e73476dde2a8034d89
Victor Porton <porton@narod.ru>
2019-12-11 16:59:06 +0200

    core/graph/connect.d

---
+++ source/xmlboiler/core/graph/connect.d
@@ -0,0 +1,42 @@
+/*
+Copyright (c) 2019 Victor Porton,
+XML Boiler - http://freesoft.portonvictor.org
+
+This file is part of XML Boiler.
+
+XML Boiler is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import xmlboiler.core.graph.relation;
+
+GraphT transitive_closure(GraphT)(GraphT graph) {
+    while (true) {
+        immutable GraphT result = union_(graph, square(graph));
+        if (result == graph) return result;
+        graph = result;
+    }
+}
+
+struct Connectivity(T) {
+    BinaryRelation!T connectivity;
+    this() {
+        connectivity = new BinaryRelation!T();
+    }
+    bool is_connected(T src, T dst) {
+        return src == dst || connectivity.adjanced(src, dst);
+    }
+    void add_relation(BinaryRelation!T relation) {
+        connectivity = transitive_closure(union_(connectivity, relation));
+    }
+}
--- source/xmlboiler/core/graph/relation.d
+++ source/xmlboiler/core/graph/relation.d
@@ -18,6 +18,8 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+module xmlboiler.core.graph.relation;
+
 /**
 Directed graph with at most one edge between vertices
 */
